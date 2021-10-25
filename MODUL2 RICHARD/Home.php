e1abd607fdcc0191c1ccd641865b353167ea724a
Akbar Affaruk Khuzaimi Ahmadani <akbaraffaruk13@gmail.com>
2021-10-25 07:31:19 +0700

    easyLah

--- MODUL2 RICHARD/Home.php
+++ MODUL2 RICHARD/Home.php
@@ -1,130 +1,130 @@
-<!doctype html>
-<html lang="en">
+    <!doctype html>
+    <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Home</title>
-    <style>
-    .navbar .container .collapse .navbar-nav {
-        margin-left: 580px;
-    }
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Home</title>
+        <style>
+        .navbar .container .collapse .navbar-nav {
+            margin-left: 580px;
+        }
 
-    h2 {
-        text-align: center;
-        margin-top: 70px;
-    }
-    </style>
-</head>
+        h2 {
+            text-align: center;
+            margin-top: 70px;
+        }
+        </style>
+    </head>
 
-<body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <body>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+            <div class="container">
+                <div class="collapse navbar-collapse" id="#collapsNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="booking.php">Booking</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <h2>WELCOME TO ESD VENUE RESERVATION</h2>
+
         <div class="container">
-            <div class="collapse navbar-collapse" id="#collapsNavbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#home">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="booking.php">Booking</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <h2>WELCOME TO ESD VENUE RESERVATION</h2>
-
-    <div class="container">
-        <section id="card" style="background-color: rgb(44, 44, 44); height: 30px; margin-top: 30px;">
-            <h5 style="text-align: center; color: white;">Find your best deal for your event here!</h5>
-        </section> <br>
-        <div class="row">
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/Nusantara Hall.jpg"
-                        class="card-img-top" alt="hotel1">
-                    <div class="card-body">
-                        <h5 class="card-title">Nusantara Hall</h5>
-                        <p class="card-text" style="margin-bottom: 0px;">$2000 / Hour</p>
-                        <p class="card-text">5000 Capacity</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">Free
-                            Parking</li>
-                        <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">Full AC
-                        </li>
-                        <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">
-                            Cleaning Service</li>
-                        <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">
-                            Covid-19 Health Protocol</li>
-                    </ul>
-                    <div class="card-footer d-flex justify-content-center">
-                        <a href="booking.php?gedung=Nusantara Hall" class="btn btn-primary">Book Now</a>
+            <section id="card" style="background-color: rgb(44, 44, 44); height: 30px; margin-top: 30px;">
+                <h5 style="text-align: center; color: white;">Find your best deal for your event here!</h5>
+            </section> <br>
+            <div class="row">
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="img/Nusantara Hall.jpg"
+                            class="card-img-top" alt="hotel1">
+                        <div class="card-body">
+                            <h5 class="card-title">Nusantara Hall</h5>
+                            <p class="card-text" style="margin-bottom: 0px;">$2000 / Hour</p>
+                            <p class="card-text">5000 Capacity</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">Free
+                                Parking</li>
+                            <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">Full AC
+                            </li>
+                            <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">
+                                Cleaning Service</li>
+                            <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">
+                                Covid-19 Health Protocol</li>
+                        </ul>
+                        <div class="card-footer d-flex justify-content-center">
+                            <a href="booking.php?gedung=Nusantara Hall" class="btn btn-primary">Book Now</a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/Garuda Hall.jpg"
-                        class="card-img-top" alt="hotel1" height="233px">
-                    <div class="card-body">
-                        <h5 class="card-title">Garuda Hall</h5>
-                        <p class="card-text" style="margin-bottom: 0px;">$1000 / Hour</p>
-                        <p class="card-text">2000 Capacity</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">Free
-                            Parking</li>
-                        <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">Full AC
-                        </li>
-                        <li class="list-group-item" style="text-align: center; color: red; font-weight: bold;">No
-                            Cleaning Service</li>
-                        <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">
-                            Covid-19 Health Protocol</li>
-                    </ul>
-                    <div class="card-footer d-flex justify-content-center">
-                        <a href="booking.php?gedung=Garuda Hall" class="btn btn-primary">Book Now</a>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="img/Garuda Hall.jpg"
+                            class="card-img-top" alt="hotel1" height="233px">
+                        <div class="card-body">
+                            <h5 class="card-title">Garuda Hall</h5>
+                            <p class="card-text" style="margin-bottom: 0px;">$1000 / Hour</p>
+                            <p class="card-text">2000 Capacity</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">Free
+                                Parking</li>
+                            <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">Full AC
+                            </li>
+                            <li class="list-group-item" style="text-align: center; color: red; font-weight: bold;">No
+                                Cleaning Service</li>
+                            <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">
+                                Covid-19 Health Protocol</li>
+                        </ul>
+                        <div class="card-footer d-flex justify-content-center">
+                            <a href="booking.php?gedung=Garuda Hall" class="btn btn-primary">Book Now</a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/Gedung Serba Guna.jpg"
-                        class="card-img-top" alt="hotel1" height="233px">
-                    <div class="card-body">
-                        <h5 class="card-title">Gedung Serba Guna</h5>
-                        <p class="card-text" style="margin-bottom: 0px;">$500 / Hour</p>
-                        <p class="card-text">500 Capacity</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item" style="text-align: center; color: red; font-weight: bold;">No Free
-                            Parking</li>
-                        <li class="list-group-item" style="text-align: center; color: red; font-weight: bold;">No Full
-                            AC</li>
-                        <li class="list-group-item" style="text-align: center; color: red; font-weight: bold;">No
-                            Cleaning Service</li>
-                        <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">
-                            Covid-19 Health Protocol</li>
-                    </ul>
-                    <div class="card-footer d-flex justify-content-center">
-                        <a href="booking.php?gedung=Gedung Serba Guna" class="btn btn-primary">Book Now</a>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="img/Gedung Serba Guna.jpg"
+                            class="card-img-top" alt="hotel1" height="233px">
+                        <div class="card-body">
+                            <h5 class="card-title">Gedung Serba Guna</h5>
+                            <p class="card-text" style="margin-bottom: 0px;">$500 / Hour</p>
+                            <p class="card-text">500 Capacity</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item" style="text-align: center; color: red; font-weight: bold;">No Free
+                                Parking</li>
+                            <li class="list-group-item" style="text-align: center; color: red; font-weight: bold;">No Full
+                                AC</li>
+                            <li class="list-group-item" style="text-align: center; color: red; font-weight: bold;">No
+                                Cleaning Service</li>
+                            <li class="list-group-item" style="text-align: center; color: green; font-weight: bold;">
+                                Covid-19 Health Protocol</li>
+                        </ul>
+                        <div class="card-footer d-flex justify-content-center">
+                            <a href="booking.php?gedung=Gedung Serba Guna" class="btn btn-primary">Book Now</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <footer style="text-align: center; background-color: rgb(214, 214, 214); height: 30px; margin-top: 50px;">Created by: richard_1202194121</footer>
+        <footer style="text-align: center; background-color: rgb(214, 214, 214); height: 30px; margin-top: 50px;">Created by: richard_1202194121</footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        </script>
 
-</body>
+    </body>
 
-</html>
+    </html>
--- MODUL2 RICHARD/my booking.php
+++ MODUL2 RICHARD/my booking.php
@@ -1,121 +1,121 @@
-<!doctype html>
-<html lang="en">
+    <!doctype html>
+    <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>My Booking</title>
-    <?php
-    $name = $_POST['name'];
-    $tanggal = $_POST['tanggal'];
-    $start = $_POST['start'];
-    $duration = $_POST['duration'];
-    $building = $_POST['building'];
-    $phone = $_POST['phone'];
-    $check_out_jam = date("H:i", strtotime("+$duration hours", strtotime($start)));
-    $check_out = date('m-d-Y H:i:s', strtotime("$tanggal $check_out_jam"));
-    $check_in = date('m-d-Y H:i:s', strtotime("$tanggal $start"));
-    $harga = 0;
-    if ($building == 'Nusantara Hall') {
-        $harga = 2000;
-    }
-    if ($building == 'Garuda Hall') {
-        $harga = 1000;
-    }
-    if ($building == 'Gedung Serba Guna') {
-        $harga = 500;
-    }
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>My Booking</title>
+        <?php
+        $name = $_GET['name'];
+        $tanggal = $_GET['tanggal'];
+        $start = $_GET['start'];
+        $duration = $_GET['duration'];
+        $building = $_GET['building'];
+        $phone = $_GET['phone'];
+        $check_out_jam = date("H:i", strtotime("+$duration hours", strtotime($start)));
+        $check_out = date('m-d-Y H:i:s', strtotime("$tanggal $check_out_jam"));
+        $check_in = date('m-d-Y H:i:s', strtotime("$tanggal $start"));
+        $harga = 0;
+        if ($building == 'Nusantara Hall') {
+            $harga = 2000;
+        }
+        if ($building == 'Garuda Hall') {
+            $harga = 1000;
+        }
+        if ($building == 'Gedung Serba Guna') {
+            $harga = 500;
+        }
 
-     if (!empty($_POST['service'])) {
-        foreach ($_POST['service'] as $data) {
-            if ($data == "Catering") {
-                $harga += 700;
-            }
-            if ($data == "Decoration") {
-                $harga += 450;
-            }
-            if ($data == "Sound System") {
-                $harga += 250;
+        if (!empty($_GET['service'])) {
+            foreach ($_GET['service'] as $data) {
+                if ($data == "Catering") {
+                    $harga += 700;
+                }
+                if ($data == "Decoration") {
+                    $harga += 450;
+                }
+                if ($data == "Sound System") {
+                    $harga += 250;
+                }
             }
         }
-    }
-    ?>
-    <style>
-    .navbar .container .collapse .navbar-nav {
-        margin-left: 580px;
-    }
-    </style>
-</head>
+        ?>
+        <style>
+        .navbar .container .collapse .navbar-nav {
+            margin-left: 580px;
+        }
+        </style>
+    </head>
 
-<body>
+    <body>
 
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-        <div class="container">
-            <div class="collapse navbar-collapse" id="#collapsNavbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="Home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="booking.php">Booking</a>
-                    </li>
-                </ul>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+            <div class="container">
+                <div class="collapse navbar-collapse" id="#collapsNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="booking.php">Booking</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    <div class="container">
-        <h4 style="text-align: center; margin-top: 70px;">
-            Thank you richard_1202194121 for Reserving</h4>
-        <h5 style="text-align: center;">Please double check for your reservation details</h5>
-        <section id="mybook">
+        <div class="container">
+            <h4 style="text-align: center; margin-top: 70px;">
+                Thank you richard_1202194121 for Reserving</h4>
+            <h5 style="text-align: center;">Please double check for your reservation details</h5>
+            <section id="mybook">
 
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Boking Number</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Check-In</th>
-                        <th scope="col">Check-Out</th>
-                        <th scope="col">Building Type</th>
-                        <th scope="col">Phone Number</th>
-                        <th scope="col">Service</th>
-                        <th scope="col">Total Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row"><?php echo rand() ?></th>
-                        <td><?php echo $name ?></td>
-                        <td><?php echo $check_in ?></td>
-                        <td><?php echo $check_out ?></td>
-                        <td><?php echo $building ?></td>
-                        <td><?php echo $phone ?></td>
-                        <td><?php
-                        if (!empty($_POST['service'])) {
-                            echo "<ul>";
-                            foreach ($_POST['service'] as $layanan) {
-                                echo "<li>" .  $layanan . "</li>";
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Boking Number</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Check-In</th>
+                            <th scope="col">Check-Out</th>
+                            <th scope="col">Building Type</th>
+                            <th scope="col">Phone Number</th>
+                            <th scope="col">Service</th>
+                            <th scope="col">Total Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"><?php echo rand() ?></th>
+                            <td><?php echo $name ?></td>
+                            <td><?php echo $check_in ?></td>
+                            <td><?php echo $check_out ?></td>
+                            <td><?php echo $building ?></td>
+                            <td><?php echo $phone ?></td>
+                            <td><?php
+                            if (empty($_POST['service'])) {
+                                echo "<ul>";
+                                foreach ($_POST['service'] as $layanan) {
+                                    echo "<li>" .  $layanan . "</li>";
+                                }
+                                echo "</ul>";
+                            } else {
+                                echo "no service";
                             }
-                            echo "</ul>";
-                        } else {
-                            echo "no service";
-                        }
-                        ?></td>
-                        <td>$ <?php echo $harga ?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
-    </div>
-    <footer style="text-align: center; background-color: rgb(214, 214, 214); height: 30px; margin-top: 410px;">Created by: richard_1202194121</footer>
+                            ?></td>
+                            <td>$ <?php echo $harga ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        </div>
+        <footer style="text-align: center; background-color: rgb(214, 214, 214); height: 30px; margin-top: 410px;">Created by: richard_1202194121</footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        </script>
 
-</body>
+    </body>
 
-</html>
+    </html>
